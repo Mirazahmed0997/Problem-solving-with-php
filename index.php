fcf8ff839e1b6157ef3fc1e087c17047afffd5e8
unknown <ahmedmiraz87@gmail.com>
2026-03-05 15:34:27 +0600

    phpProblemSolving

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$name='Miraz';
+$language="PHP";
+
+echo "Hello $name! This is my first $language project! <br>";
+
+function ageDetector(int $age)
+{
+   if($age >= 18)
+    {
+        echo "Congrtulations! You are an adult <br>";
+    }
+    else{
+        echo "opps ! you a an underage person <br>";
+    }
+}
+echo ageDetector(18);
+
+
+$age=144;
+$text=match (true) {
+    $age < 18 => "You are an under age <br>",
+    $age >= 18 =>"You are an adult <br>" ,
+};
+
+echo $text;
+
+
+for($x=1; $x<=10; $x++)
+    {
+        if($x%2==0)
+            {
+                echo $x." : ". "Number is Even <br>";
+            }
+            else echo $x." : ". "Number is odd <br>";
+    }
+
+?>
