6ca46f4b7095353557ed0a4089929fcf1fbbdd6b
unknown <ahmedmiraz87@gmail.com>
2026-03-07 12:03:03 +0600

    PushValueInarray

--- index.php
+++ index.php
@@ -1,55 +1,56 @@
 <?php
 
-$name='Miraz';
-$language="PHP";
+$name = 'Miraz';
+$language = "PHP";
 
 echo "Hello $name! This is my first $language project! <br>";
 
 function ageDetector(int $age)
 {
-   if($age >= 18)
-    {
+    if ($age >= 18) {
         echo "Congrtulations! You are an adult <br>";
-    }
-    else{
+    } else {
         echo "opps ! you a an underage person <br>";
     }
 }
 echo ageDetector(18);
 
 
-$age=144;
-$text=match (true) {
+$age = 144;
+$text = match (true) {
     $age < 18 => "You are an under age <br>",
-    $age >= 18 =>"You are an adult <br>" ,
+    $age >= 18 => "You are an adult <br>",
 };
 
 echo $text;
 
 
-for($x=1; $x<=10; $x++)
-    {
-        if($x%2==0)
-            {
-                echo $x." : ". "Number is Even <br>";
-            }
-            else echo $x." : ". "Number is odd <br>";
-    }
+for ($x = 1; $x <= 10; $x++) {
+    if ($x % 2 == 0) {
+        echo $x . " : " . "Number is Even <br>";
+    } else
+        echo $x . " : " . "Number is odd <br>";
+}
 
 
-    $cars = array("Volvo", "BMW", "Toyota");
+$cars = array("Volvo", "BMW", "Toyota");
 
-    for($j=0;$j < count($cars) ;$j++)
-        {
-            echo ($j + 1)." : ". $cars[$j]."<br>"; 
-        }
+for ($j = 0; $j < count($cars); $j++) {
+    echo ($j + 1) . " : " . $cars[$j] . "<br>";
+}
 
 
 
-    $employees= ["miraz","shahed","Alif"];
-    foreach($employees as $employee)
-        {
-            echo $employee. "<br>";
-        }
+$employees = ["miraz", "shahed", "Alif"];
+foreach ($employees as $employee) {
+    echo $employee . "<br>";
+}
+
+array_push($employees, "ikbal", "Hossain");
+
+echo "After added New Value " . "<br>";
+foreach ($employees as $employee) {
+    echo $employee . "<br>";
+}
 
 ?>
